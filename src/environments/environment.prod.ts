094d6014a924c7c85ecd736921b9bcc57e686e18
asmabouchoucha <asmabouchoucha28@gmail.com>
2020-04-20 03:27:47 +0200

    Not so PROD ENV

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,35 @@
 export const environment = {
-  production: true
+  production: true?
+  neo4jConfig : {
+    url: 'bolt://localhost:7687',
+    user: 'admin',
+    password: 'admin',
+    driver: {
+      // all the driver configuration (optional)
+    }
+  },
+  neo4jStyle : {
+    nodes: { // Map of label
+      NETWORK: {
+        color: '#ff0066', // Color of the node
+        size: 50, // Size of the node
+      },
+      APPLICATION: {
+        color: '#0000ff', 
+        size: 50,
+      },
+      AGENCE: {
+        color: '#8000ff', 
+        size: 50,
+      },
+      COLLABORATEUR: {
+        color: '#d0d0e1', 
+        size: 50,
+      }
+    },
+    edges: {
+      color: '#d9d9d9',
+      size: 1,
+    }
+  }
 };
